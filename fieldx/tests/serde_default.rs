bfb5dee853e2e5a3daaab0602b983c4c085b3594
Vadim Belman <vrurg@users.noreply.github.com>
2024-05-25 17:57:33 -0400

    Add more test cases for serialization struct-level default

--- fieldx/tests/serde_default.rs
+++ fieldx/tests/serde_default.rs
@@ -37,6 +37,35 @@ impl Bar {
     }
 }
 
+#[fxstruct(sync, get, serde("BazDup", default(Self::serde_default())))]
+#[derive(Clone)]
+struct Baz {
+    #[fieldx(reader)]
+    f1: String,
+    f2: String,
+}
+
+impl Baz {
+    fn serde_default() -> Fubar {
+        Fubar {
+            postfix: "from fubar".into(),
+        }
+    }
+}
+
+struct Fubar {
+    postfix: String,
+}
+
+impl From<Fubar> for BazDup {
+    fn from(value: Fubar) -> Self {
+        Self {
+            f1: format!("f1 {}", value.postfix),
+            f2: format!("f2 {}", value.postfix),
+        }
+    }
+}
+
 #[test]
 fn missing_key_meta() {
     let json_src = r#"{"f2": "f2 json"}"#;
@@ -58,5 +87,17 @@ fn missing_key_str() {
 
     let json_src = r#"{"f2": "f2 json", "f1": "f1 json"}"#;
     let foo_de = serde_json::from_str::<Bar>(&json_src).expect("Bar deserialization failure");
+    assert_eq!(foo_de.f1(), "f1 json".to_string());
+}
+
+#[test]
+fn missing_with_3rd_party() {
+    let json_src = r#"{"f1": "f1 json"}"#;
+    let foo_de = serde_json::from_str::<Baz>(&json_src).expect("Bar deserialization failure");
+    assert_eq!(foo_de.f1(), "f1 json".to_string());
+    assert_eq!(foo_de.f2(), "f2 from fubar".to_string());
+
+    let json_src = r#"{"f2": "f2 json", "f1": "f1 json"}"#;
+    let foo_de = serde_json::from_str::<Baz>(&json_src).expect("Bar deserialization failure");
     assert_eq!(foo_de.f1(), "f1 json".to_string());
 }
