3a421b7694cf4934bb00ee146ac34f2430f52c58
Vadim Belman <vrurg@users.noreply.github.com>
2024-11-26 10:58:02 -0500

    Implement FXOrig for optional types
    Also renamed method `span` into `orig_span` and added `fx_span`
    which returns non-optional span.

--- fieldx_aux/src/with_origin.rs
+++ fieldx_aux/src/with_origin.rs
@@ -9,7 +9,29 @@ where
     fn orig(&self) -> Option<&O>;
 
     #[allow(dead_code)]
-    fn span(&self) -> Option<Span> {
+    fn orig_span(&self) -> Option<Span> {
         self.orig().and_then(|o| Some(o.span()))
+    }
+
+    fn fx_span(&self) -> Span {
+        self.orig_span().unwrap_or_else(|| Span::call_site())
+    }
+}
+
+impl<O, T> FXOrig<O> for Option<T>
+where
+    O: Spanned,
+    T: FXOrig<O>,
+{
+    fn orig(&self) -> Option<&O> {
+        self.as_ref().and_then(|s| s.orig())
+    }
+
+    fn orig_span(&self) -> Option<Span> {
+        self.as_ref().and_then(|s| s.orig_span())
+    }
+
+    fn fx_span(&self) -> Span {
+        self.as_ref().map_or_else(|| Span::call_site(), |s| s.fx_span())
     }
 }
