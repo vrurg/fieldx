31e0743f41239b195eb7fe746f49df378de8ae5e
Vadim Belman <vrurg@users.noreply.github.com>
2025-06-28 16:55:54 -0400

    Release fieldx_derive version 0.2.1

--- fieldx_derive/src/lib.rs
+++ fieldx_derive/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/fieldx_derive/0.2.0/")]
+#![doc(html_root_url = "https://docs.rs/fieldx_derive/0.2.1/")]
 //! The core functionality of [FieldX](https://docs.rs/fieldx/latest/fieldx/). This crate provides the `fxstruct`
 //! procedural macro that implements the code‐generation logic.
 
